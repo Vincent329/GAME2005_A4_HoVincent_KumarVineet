edd47a05c8ea900ada3ab7214f7bbbebfc2d5d52
Vineet Kumar <vineet.z91@gmail.com>
2020-12-02 00:17:25 -0500

    Start Scene and Play Scene added
    Start Scene has Text for Student Name and IDs, Assignment Number, Start button (with highlights) for script to go to the Play Scene.

---
+++ Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // All scripts have these functions, Awake() was added, but commenting these are it's not required for the Start Scene  - Vineet
+
+    /*void Awake() {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    public void  GoToPlay()
+    {
+       UnityEngine.SceneManagement.SceneManager.LoadScene("PlayScene", LoadSceneMode.Single);
+    }
+
+    
+}
