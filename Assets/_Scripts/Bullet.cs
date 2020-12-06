6325c90274cb1b87ba7a3c9b7ecbb2297528957d
NickW <nickmwootton@gmail.com>
2020-12-06 12:51:20 -0500

    Added Bullet and Cannon
    Attempted to make a bullet and a cannon, the bullet has basic movement and the cannon has nothing in it pretty much, idk what I'm doing

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float groundLevel = GameObject.Find("Floor").transform.position.y;
+    public Vector3 m_velocity, m_acceleration;
+
+    private bool m_active;
+    private float deltaTime = 1.0f / 60.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_active)
+        {
+            Move();
+            CheckCollision();
+        }
+    }
+
+    void Move()
+    {
+        m_velocity += m_acceleration;
+        transform.position += m_velocity * deltaTime;
+    }
+
+    void CheckCollision()
+    {
+        if(transform.position.y <= groundLevel)
+        {
+            m_velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            m_acceleration = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3(0.0f, groundLevel - 5.0f, 0.0f);
+        m_velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        m_acceleration = new Vector3(0.0f, -0.98f, 0.0f);
+        m_active = false;
+    }
+    void Spawn()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/Cannon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public static BulletPool bulletPoolRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bulletPoolRef = BulletPool.GetBulletPool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Fire()
+    {
+        //bulletPoolRef.FireBullet(transform.position);
+    }
+}
